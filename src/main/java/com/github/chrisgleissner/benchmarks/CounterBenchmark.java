a46d0cb8b3702542b0adf547d8287a2d1eb3b48f
Christian Gleissner <chrisgleissner@gmail.com>
2018-11-12 23:25:06 +0000

    Extended counter benchmark

--- src/main/java/com/github/chrisgleissner/benchmarks/CounterBenchmark.java
+++ src/main/java/com/github/chrisgleissner/benchmarks/CounterBenchmark.java
@@ -1,12 +1,14 @@
 package com.github.chrisgleissner.benchmarks;
 
 import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-@BenchmarkMode(Mode.Throughput)
+@BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
 @Warmup(iterations = 5, time = 2)
 @Measurement(iterations = 5, time = 3)
 @Fork(2)
@@ -18,27 +20,53 @@ public class CounterBenchmark {
         @Setup(Level.Trial)
         public void setUp() {
             i = 0;
-            atomicInteger = new AtomicInteger();
+            l = 0;
+
+            atomicInt = new AtomicInteger();
+            atomicLong = new AtomicLong();
+
             mutableInt = new MutableInt();
+            mutableLong = new MutableLong();
         }
 
         public int i;
-        public AtomicInteger atomicInteger;
+        public long l;
+
+        public AtomicInteger atomicInt;
+        public AtomicLong atomicLong;
+
         public MutableInt mutableInt;
+        public MutableLong mutableLong;
     }
 
+
     @Benchmark
-    public void countInt(Blackhole blackhole, MyState state) {
+    public void primitiveInt(Blackhole blackhole, MyState state) {
         blackhole.consume(state.i++);
     }
 
     @Benchmark
-    public void countAtomicInteger(Blackhole blackhole, MyState state) {
-        blackhole.consume(state.atomicInteger.getAndIncrement());
+    public void primitiveLong(Blackhole blackhole, MyState state) {
+        blackhole.consume(state.l++);
     }
 
     @Benchmark
-    public void countMutableInt(Blackhole blackhole, MyState state) {
+    public void atomicInteger(Blackhole blackhole, MyState state) {
+        blackhole.consume(state.atomicInt.getAndIncrement());
+    }
+
+    @Benchmark
+    public void atomicLong(Blackhole blackhole, MyState state) {
+        blackhole.consume(state.atomicLong.getAndIncrement());
+    }
+
+    @Benchmark
+    public void mutableInt(Blackhole blackhole, MyState state) {
         blackhole.consume(state.mutableInt.getAndIncrement());
+    }
+
+    @Benchmark
+    public void mutableLong(Blackhole blackhole, MyState state) {
+        blackhole.consume(state.mutableLong.getAndIncrement());
     }
 }
