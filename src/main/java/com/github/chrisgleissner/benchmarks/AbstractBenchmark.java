be52bc6ac869974995a9b2a9c2056e31a50df992
Christian Gleissner <chrisgleissner@gmail.com>
2019-01-21 23:39:50 +0000

    Updated benchmarks to use throughput

--- src/main/java/com/github/chrisgleissner/benchmarks/AbstractBenchmark.java
+++ src/main/java/com/github/chrisgleissner/benchmarks/AbstractBenchmark.java
@@ -7,12 +7,10 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Warmup;
 
-import java.util.concurrent.TimeUnit;
-
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(MILLISECONDS)
 @Warmup(iterations = 15, time = 300, timeUnit = MILLISECONDS)
 @Measurement(iterations = 15, time = 300, timeUnit = MILLISECONDS)
 @Fork(1)
