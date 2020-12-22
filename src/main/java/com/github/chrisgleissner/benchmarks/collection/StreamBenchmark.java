14088d4b352d45084def3db2cfe443f69e6fa3ad
lkryukova <lkryukova@azul.com>
2020-12-22 00:05:11 -0800

    Stream stabilization
    Problems:
    1. Using a random number as a thrashhold is an extremely unstable way to estimate. Example: take 1,000,000 numbers and for the first iteration add up all to a random number which turned out to be 10, and on the second it turned out to be 10,000, respectively, and the time on the iteration will differ and variation per iterations jumps
    2. Using 1 number to compose an array takes place. But since we operate with large numbers and the largest in this chain do not have time to pass in 1 second. It will be more convenient and efficient to measure the average time. that is, to evaluate not on one number, but to use the range for the trashholds at once and evaluate the average of them. We took it depending on the numbers from 1% for little to 50% for huge ones.
    3. Shuffle destabilizes the program only because when adding values, they are stored side by side. But by mixing them, he mixes them in memory, so each time we have a different distance between the necessary cells in memory. That would not remove the possibility of mixing numbers. On the setup, after mixing, we added a pass to stabilize the values in memory, so that they would be consistently next to each other.
    4. Make a setup at each iteration on which each time both the array shuffle and the threshhold change, see the description above - this is not advisable from the point of view of variation, which turns out to be more than 100% in many cases. Accordingly, the setup before all iterations reduced the variation several times.
    5. Some cases remained unchanged because they are not variable.

--- src/main/java/com/github/chrisgleissner/benchmarks/collection/StreamBenchmark.java
+++ src/main/java/com/github/chrisgleissner/benchmarks/collection/StreamBenchmark.java
@@ -5,7 +5,7 @@ import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -17,12 +17,16 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
 
+@State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
 @OperationsPerInvocation(1)
 public class StreamBenchmark extends AbstractCollectionBenchmark {
 
@@ -50,7 +54,6 @@ public class StreamBenchmark extends AbstractCollectionBenchmark {
     public int integerStreamReduceToSum(MyState state) {
         return state.integers.stream().reduce(0, (i, j) -> i + j);
     }
-
     @Benchmark
     public int integerStreamReduceToSumParallel(MyState state) {
         return state.integers.stream().parallel().reduce(0, (i, j) -> i + j);
@@ -68,47 +71,69 @@ public class StreamBenchmark extends AbstractCollectionBenchmark {
 
     @Benchmark
     public void integerStreamForEach(MyState state, Blackhole blackhole) {
-        Collection<Integer> target = new ArrayList<>();
-        state.integers.stream().filter(i -> i < state.integerFilter).forEach(target::add);
+        final Collection<Integer> target = new ArrayList<>();
+        for (Integer threshold : state.range) {
+            state.integers.stream().filter(i -> i < threshold).forEach(target::add);
+        }
         blackhole.consume(target);
     }
 
     @Benchmark
     public void integerStreamForEachParallel(MyState state, Blackhole blackhole) {
-        Set<Integer> target = new ConcurrentSkipListSet<>();
-        state.integers.stream().parallel().filter(i -> i < state.integerFilter).forEach(target::add);
+        final Set<Integer> target = new ConcurrentSkipListSet<>();
+        for (Integer threshold : state.range) {
+            state.integers.stream().parallel().filter(i -> i < threshold).forEach(target::add);
+        }
         blackhole.consume(target);
     }
 
     @Benchmark
     public void integerStreamCollect(MyState state, Blackhole blackhole) {
-        blackhole.consume(state.integers.stream().filter(i -> i < state.integerFilter).collect(toList()));
+        Collection<Integer> target = new ArrayList<>();
+        for (Integer threshold : state.range) {
+            target = state.integers.stream().filter(i -> i < threshold).collect(toList());
+        }
+        blackhole.consume(target);
     }
 
     @Benchmark
     public void integerStreamCollectParallel(MyState state, Blackhole blackhole) {
-        blackhole.consume(state.integers.stream().parallel().filter(i -> i < state.integerFilter).collect(toList()));
+        Collection<Integer> target = new ArrayList<>();
+        for (Integer threshold : state.range) {
+           target = state.integers.stream().parallel().filter(i -> i < threshold).collect(toList());
+        }
+        blackhole.consume(target);
     }
 
     @Benchmark
     public void integerFor(MyState state, Blackhole blackhole) {
         Collection<Integer> source = state.integers;
         Collection<Integer> target = new ArrayList<>();
-        Integer filter = state.integerFilter;
-        for (Integer i : source)
-            if (i < filter)
-                target.add(i);
+        for (Integer threshold : state.range) {
+            Integer filter = threshold;
+            for (Integer i : source)
+                if (i < filter)
+                    target.add(i);
+        }
         blackhole.consume(target);
     }
 
     @Benchmark
     public void intStream(MyState state, Blackhole blackhole) {
-        blackhole.consume(Arrays.stream(state.ints).filter(i -> i < state.intFilter).toArray());
+        int[] target = new int[state.collectionSize];
+        for (Integer threshold : state.range) {
+            target = Arrays.stream(state.ints).filter(i -> i < threshold).toArray();
+        }
+        blackhole.consume(target);
     }
 
     @Benchmark
     public void intStreamParallel(MyState state, Blackhole blackhole) {
-        blackhole.consume(Arrays.stream(state.ints).parallel().filter(i -> i < state.intFilter).toArray());
+        int[] target = new int[state.collectionSize];
+        for (Integer threshold : state.range) {
+            Arrays.stream(state.ints).parallel().filter(i -> i < threshold).toArray();
+        }
+        blackhole.consume(target);
     }
 
     @Benchmark
@@ -116,11 +141,14 @@ public class StreamBenchmark extends AbstractCollectionBenchmark {
         int[] source = state.ints;
         int[] target = new int[source.length];
         int targetIndex = 0;
-        int filter = state.intFilter;
-        for (int i = 0; i < source.length; i++) {
-            int candidate = source[i];
-            if (candidate < filter)
-                target[targetIndex++] = candidate;
+        for (Integer threshold : state.range) {
+            targetIndex = 0;
+            int filter = threshold;
+            for (int i = 0; i < source.length; i++) {
+                int candidate = source[i];
+                if (candidate < filter)
+                    target[targetIndex++] = candidate;
+            }
         }
         int[] trimmedTarget = new int[targetIndex];
         System.arraycopy(source, 0, trimmedTarget, 0, targetIndex);
@@ -131,20 +159,36 @@ public class StreamBenchmark extends AbstractCollectionBenchmark {
     public static class MyState {
         static Random random = new Random();
         public List<Integer> integers;
+        public List<Integer> range;
         public int[] ints;
-        public Integer integerFilter;
-        public int intFilter;
-        @Param({"1", "10", "100", "1000", "10000", "100000", "1000000", "10000000"})
+
+        @Param({"1", "10", "100", "1000", "10000", "100000","1000000", "10000000"})
         int collectionSize;
 
-        @Setup(Level.Iteration)
+        @Setup(Level.Trial)
         public void doSetup() {
-            integerFilter = random.nextInt(collectionSize);
-            intFilter = integerFilter;
-
+            if (collectionSize <= 100) {
+                range = range(0, collectionSize).boxed().collect(toList());
+            }
+            else if (collectionSize <= 100000) {
+                int step = collectionSize / 100;
+                range = range(0, 100).map(x -> x * step).boxed().collect(toList());
+            }
+            else {
+                int step = collectionSize / 2;
+                range = range(0, 2).map(x -> x * step).boxed().collect(toList());
+            }
             integers = range(0, collectionSize).boxed().collect(toList());
             Collections.shuffle(integers, random);
             ints = integers.stream().mapToInt(Integer::valueOf).toArray();
+
+            // Fix data layout so that all elements are plced continuously in memory (not in random order ad after shuffle)
+            List<Integer> old_int = integers;
+            integers = new ArrayList<Integer>(old_int.size());
+            for (Integer i: old_int) {
+                integers.add(Integer.valueOf(i.intValue()));
+            }
         }
     }
+
 }
